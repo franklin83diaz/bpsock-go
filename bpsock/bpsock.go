2666c8debf3696681cea3a819c3eb8e9ccd6141c
Franklin Diaz <franklin83diaz@gmail.com>
2024-03-28 07:57:14 -0400

    change Bpsock to BpSock

--- bpsock/bpsock.go
+++ bpsock/bpsock.go
@@ -6,20 +6,22 @@ import (
 )
 
 // Bpsock
-type Bpsock struct {
+type BpSock struct {
 	socket net.Conn
 	dmtu   int
 }
 
-func (bpsock *Bpsock) New(socket net.Conn, dmtu ...int) {
+func (bpsock *BpSock) New(socket net.Conn, dmtu ...int) {
+
 	bpsock.dmtu = 15000000
 	if len(dmtu) > 0 {
 		bpsock.dmtu = dmtu[0]
 	}
 	bpsock.socket = socket
+
 }
 
-func (bpsock *Bpsock) send(data []byte, tag tags.Tag16) error {
+func (bpsock *BpSock) send(data []byte, tag tags.Tag16) error {
 	// !TODO:
 	//lock up channel if it is busy
 	//icrement channel counter
